f363721b6b1acf601ba44f6aa1287f938f406516
Nehemiah I. Dacres <vivacarlie@gmail.com>
2020-07-15 23:31:56 -0500

    changed ProjectList to ProjectListEntry

--- src/components/ProjectList.vue
+++
@@ -1,9 +0,0 @@
-<section>
-  <h1>Projects</h1>
-  <ol id='project-list'>
-    <li v-for "repo in repos">
-      <p class='name' > {{repo.name}} </p>
-      <p class='description'> {{repo.description}} </p>
-    </li>
-  </ol>
-</section>
